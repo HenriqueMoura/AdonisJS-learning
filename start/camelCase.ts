439dac6197c3e5feed3101c49c9e9d5dda65d64b
Henrique Almeida <hamoura@recordtv.com.br>
2023-01-30 20:41:06 -0300

    modify: change type return API key to camelCase

---
+++ start/camelCase.ts
@@ -0,0 +1,7 @@
+import {BaseModel} from '@ioc:Adonis/Lucid/Orm'
+import {string} from '@ioc:Adonis/Core/Helpers'
+
+
+BaseModel.namingStrategy.serializedName = ( _model, key)=> {
+  return  string.camelCase(key)
+}
