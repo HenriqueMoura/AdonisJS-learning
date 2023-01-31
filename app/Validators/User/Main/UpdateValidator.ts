8f3e138d554a5057d9c02112ba3ef53f8a5f2175
Henrique Almeida <hamoura@recordtv.com.br>
2023-01-31 00:39:58 -0300

    feat: User update infos

---
+++ app/Validators/User/Main/UpdateValidator.ts
@@ -0,0 +1,13 @@
+import { schema, CustomMessages, rules } from '@ioc:Adonis/Core/Validator'
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+export default class StoreValidator {
+  constructor(protected ctx: HttpContextContract) {}
+  public schema = schema.create({
+    username: schema.string.optional({trim: true}),
+    name: schema.string.optional({ trim: true }),
+    password: schema.string.optional({ trim: true }, [rules.confirmed('passwordConfirmation')]),
+  })
+
+  public messages: CustomMessages = {}
+}
