b1ad4f4eee4c945bec92899b6e10f813eb0bdfb9
Henrique Almeida <hamoura@recordtv.com.br>
2023-02-09 15:11:33 -0300

    deploy

--- start/routes/users.ts
+++ start/routes/users.ts
@@ -8,7 +8,7 @@ Route.post('/users/forgot-password', 'Users/ForgotPassword.store')
 Route.get('/users/forgot-password/:key', 'Users/ForgotPassword.show')
 Route.put('/users/forgot-password', 'Users/ForgotPassword.update')
 
-Route.get('/users', 'Users/Main.show').middleware('acl:admin')
+Route.get('/users', 'Users/Main.show').middleware('auth')
 Route.put('/users', 'Users/Main.update').middleware('auth')
 
 
