ed84fa15f370c1bbf404f4a6900a781b2bc27020
Henrique Moura <henrique.almeida.moura@gmail.com>
2023-02-24 22:32:34 +0000

    fix: lint

--- database/migrations/1676520681237_group_students.ts
+++ database/migrations/1676520681237_group_students.ts
@@ -3,19 +3,22 @@ import BaseSchema from '@ioc:Adonis/Lucid/Schema'
 export default class GroupStudent extends BaseSchema {
   protected tableName = 'group_student'
 
-  public async up () {
+  public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.integer('group_id').unsigned().references('id').inTable('groups')
-      table.integer('user_id').unsigned().references('id').inTable('users')
+      table
+        .integer('user_id')
+        .unsigned()
+        .references('id')
+        .inTable('users')
         .onUpdate('CASCADE')
         .onDelete('CASCADE')
-
 
       table.timestamps(true, true)
     })
   }
 
-  public async down () {
+  public async down() {
     this.schema.dropTable(this.tableName)
   }
 }
